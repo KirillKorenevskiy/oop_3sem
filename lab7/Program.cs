2a4f08f20617e71fb78bf37312b792d0db28b217
KirillKorenevskiy <118692323+KirillKorenevskiy@users.noreply.github.com>
2024-03-09 18:33:59 +0300

    Add files via upload

---
+++ lab7/Program.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+
+public interface ICollectionType<T>
+{
+    void Add(T element);
+    void Remove(T element);
+    void Display();
+    T Find(Predicate<T> predicate);
+}
+
+public class Set<T> : ICollectionType<T> where T : IComparable<T>
+{
+    private List<T> elements;
+
+    public Set()
+    {
+        elements = new List<T>();
+    }
+
+    public Set(IEnumerable<T> collection)
+    {
+        elements = new List<T>(collection);
+    }
+
+    public void Add(T element)
+    {
+        if (!elements.Contains(element))
+        {
+            elements.Add(element);
+        }
+    }
+
+    public void Remove(T element)
+    {
+        elements.Remove(element);
+    }
+
+    public void Display()
+    {
+        foreach (T element in elements)
+        {
+            if (element is Examination examination)
+            {
+                examination.Display();
+            }
+            else
+            {
+                Console.WriteLine(element);
+            }
+        }
+    }
+
+    public T Find(Predicate<T> predicate)
+    {
+        try
+        {
+            return elements.Find(predicate);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Ошибка при поиске элемента: " + ex.Message);
+            return default(T);
+        }
+        finally
+        {
+            Console.WriteLine("Блок finally");
+        }
+    }
+
+    public int Length()
+    {
+        return elements.Count;
+    }
+
+    public override string ToString()
+    {
+        return string.Join(", ", elements);
+    }
+
+
+    public void SaveToJsonFile(string filePath)
+    {
+        string json = JsonConvert.SerializeObject(elements, Formatting.Indented);
+        File.WriteAllText(filePath, json);
+    }
+
+    public void LoadFromJsonFile(string filePath)
+    {
+        if (File.Exists(filePath))
+        {
+            string json = File.ReadAllText(filePath);
+            elements = JsonConvert.DeserializeObject<List<T>>(json);
+        }
+    }
+}
+
+public class Examination : IComparable<Examination>
+{
+    public string Subject { get; set; }
+    public DateTime Date { get; set; }
+    public string Location { get; set; }
+
+    public Examination(string subject, DateTime date, string location)
+    {
+        Subject = subject;
+        Date = date;
+        Location = location;
+    }
+
+    public void Display()
+    {
+        Console.WriteLine($"Предмет: {Subject}, Дата: {Date}, Место: {Location}");
+    }
+
+    public int CompareTo(Examination other)
+    {
+        return Date.CompareTo(other.Date);
+    }
+}
+
+
+class Program
+{
+    static void Main()
+    {
+        Set<int> intSet = new Set<int>();
+        intSet.Add(1);
+        intSet.Add(2);
+        intSet.Add(3);
+        intSet.Display();
+
+        Set<double> doubleSet = new Set<double>();
+        doubleSet.Add(1.5);
+        doubleSet.Add(2.5);
+        doubleSet.Add(3.5);
+        doubleSet.Display();
+
+        Set<string> stringSet = new Set<string>();
+        stringSet.Add("Я");
+        stringSet.Add("Кирилл");
+        stringSet.Display();
+
+        int foundInt = intSet.Find(x => x == 2);
+        Console.WriteLine("Найден int: " + foundInt);
+
+        double foundDouble = doubleSet.Find(x => x > 2.0);
+        Console.WriteLine("Найден double: " + foundDouble);
+
+        string foundString = stringSet.Find(x => x.StartsWith("W"));
+        Console.WriteLine("Найден string: " + foundString);
+
+        Console.WriteLine();    
+
+        Set<Examination> examinationSet = new Set<Examination>();
+        examinationSet.Add(new Examination("Математика", new DateTime(2023, 10, 1), "Аудитория 101"));
+        examinationSet.Add(new Examination("Физика", new DateTime(2023, 10, 5), "Аудитория 202"));
+        examinationSet.Display();
+
+
+        Set<Examination> collection = new Set<Examination>();
+        string filePath = "data.json";
+        collection.SaveToJsonFile(filePath);
+        collection.LoadFromJsonFile(filePath);
+    }
+}
---
+++ lab8/Program.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Linq;
+
+
+public class Boss
+{
+    public event Action Upgrade;
+    public event Action<int> TurnOn;
+
+    public void TriggerUpgrade()
+    {
+        Upgrade?.Invoke();
+    }
+
+    public void TriggerTurnOn(int voltage)
+    {
+        TurnOn?.Invoke(voltage);
+    }
+}
+
+public abstract class Device
+{
+    public string Name { get; set; }
+
+    public Device(string name)
+    {
+        Name = name;
+    }
+
+    public abstract void ReactToUpgrade();
+
+    public abstract void ReactToTurnOn(int voltage);
+}
+
+public class Laptop : Device
+{
+    public Laptop(string name) : base(name)
+    {
+    }
+
+    public override void ReactToUpgrade()
+    {
+        Console.WriteLine($"{Name} получил обновление. Теперь он работает быстрее.");
+    }
+
+    public override void ReactToTurnOn(int voltage)
+    {
+        if (voltage < 100)
+        {
+            Console.WriteLine($"{Name} не может включиться из-за низкого напряжения.");
+        }
+        else
+        {
+            Console.WriteLine($"{Name} успешно включился.");
+        }
+    }
+}
+
+public class Robot : Device
+{
+    public Robot(string name) : base(name)
+    {
+    }
+
+    public override void ReactToUpgrade()
+    {
+        Console.WriteLine($"{Name} обновил свое программное обеспечение. Появились новые возможности.");
+    }
+
+    public override void ReactToTurnOn(int voltage)
+    {
+        if (voltage > 200)
+        {
+            Console.WriteLine($"{Name} перегрузился и сломался.");
+        }
+        else
+        {
+            Console.WriteLine($"{Name} включился.");
+        }
+    }
+}
+
+
+class StringProcessor
+{
+    public static void ProcessString(string input)
+    {
+        Console.WriteLine("Исходная строка: " + input);
+    }
+
+    public static void RemovePunctuation(string input)
+    {
+        string result = new string(input.ToCharArray()
+            .Where(c => !char.IsPunctuation(c))
+            .ToArray());
+
+        Console.WriteLine("Строка после удаления знаков препинания: " + result);
+    }
+
+    public static void AddSymbols(string input)
+    {
+        string result = input + "!!!";
+
+        Console.WriteLine("Строка после добавления символов: " + result);
+    }
+
+    public static void ConvertToUpper(string input)
+    {
+        string result = input.ToUpper();
+
+        Console.WriteLine("Строка после замены на заглавные буквы: " + result);
+    }
+
+    public static void RemoveExtraSpaces(string input)
+    {
+        string result = string.Join(" ", input.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries));
+
+        Console.WriteLine("Строка после удаления лишних пробелов: " + result);
+    }
+    public static void ReplaceSubstring(string input)
+    {
+        string result =  input.Replace("строки", "Кирилл");
+        Console.WriteLine("Строка после замены слов: " + result);
+    }
+}
+
+
+
+public class Program
+{
+    static void Main()
+    {
+        Boss boss = new Boss();
+
+        Laptop laptop1 = new Laptop("Ноутбук 1");
+        laptop1.ReactToUpgrade();
+
+        Robot robot1 = new Robot("Робот 1");
+        robot1.ReactToTurnOn(150);
+
+        Laptop laptop2 = new Laptop("Ноутбук 2");
+        Robot robot2 = new Robot("Робот 2");
+
+        boss.Upgrade += () => laptop1.ReactToUpgrade();
+        boss.Upgrade += () => robot1.ReactToUpgrade();
+        boss.Upgrade += () => Console.WriteLine("Универсальное устройство получило обновление.");
+
+        boss.TurnOn += (voltage) => laptop2.ReactToTurnOn(voltage);
+        boss.TurnOn += (voltage) => robot2.ReactToTurnOn(voltage);
+
+        boss.TriggerUpgrade();
+        boss.TriggerTurnOn(120);
+        boss.TriggerTurnOn(250);
+
+        Console.WriteLine();
+        Console.WriteLine("------------------Задание 2--------------------");
+        Console.WriteLine();
+
+        string input = "Пример строки для обработки.";
+
+        Action<string> stringProcessing = StringProcessor.ProcessString;
+
+        stringProcessing += StringProcessor.RemovePunctuation;
+        stringProcessing += StringProcessor.AddSymbols;
+        stringProcessing += StringProcessor.ConvertToUpper;
+        stringProcessing += StringProcessor.RemoveExtraSpaces;
+        stringProcessing += StringProcessor.ReplaceSubstring;
+
+        stringProcessing(input);
+    }
+}
